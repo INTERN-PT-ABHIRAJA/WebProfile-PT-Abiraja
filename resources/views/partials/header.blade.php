f500f1d5d2d6654e96d3e95f728fd420453d1b92
ABintangIftitahFj <bintangiftitahf28@upi.edu>
2025-05-10 17:00:53 +0700

    Add modals for About, Contact, and Product details; implement footer and header sections; create various content sections including About, Hero, Partners, Products, and Services.

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,52 @@
+@extends('welcome')
+
+@section('content')
+<header>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="assets/img/logo/Logo.png" alt="Logo" class="img-fluid">
+            </a>
+            <div class="d-none d-lg-block d-md-block head">
+                <h6 class="text-black fw-bold m-0 p-0">PT ABHIRAJA GIOVANNI TRYAMANDA</h6>
+            </div>
+            <div class="d-sm-block d-md-none d-lg-none head">
+                <h6 class="text-black fw-bold m-0 p-0">PT ABHIRAJA GIOVANNI T</h6>
+            </div>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title">Menu</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav ms-auto w-100 justify-content-end">
+                        <li class="nav-item">
+                            <a class="nav-link text-black fw-bold" href="#home">BERANDA</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black fw-bold" href="#about">TENTANG KAMI</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black fw-bold" href="#services">TESTIMONI</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black fw-bold" href="#team">MITRA</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black fw-bold" href="#products">LAYANAN KAMI</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black fw-bold" href="#contact">KONTAK</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+@endsection
---
+++ resources/views/section/services.blade.php
@@ -0,0 +1,75 @@
+@extends('welcome')
+@section ('content')
+<section class="contact-section" id="contact">
+            <div class="container">
+                <div class="text-center mb-5" data-aos="fade-up">
+                    <h2 class="fw-bold mb-3">Hubungi Kami</h2>
+                    <p class="text-white-50 mx-auto" style="max-width: 700px;">Jangan ragu untuk menghubungi kami jika Anda memiliki pertanyaan atau ingin bekerja sama.</p>
+                </div>
+                
+                <div class="row">
+                    <div class="col-lg-5" data-aos="fade-right">
+                        <div class="contact-info">
+                            <div class="contact-item">
+                                <div class="contact-icon">
+                                    <i class="fas fa-map-marker-alt"></i>
+                                </div>
+                                <div class="contact-text">
+                                    <h5>Alamat</h5>
+                                    <p>Jl. Alamanda 7 No. 39 Jatinangor, Sumedang
+                                        Jl. Raya Buahdua 2, Kec. Buahdua Sumedang</p>
+                                </div>
+                            </div>
+                            
+                            <div class="contact-item">
+                                <div class="contact-icon">
+                                    <i class="fas fa-phone-alt"></i>
+                                </div>
+                                <div class="contact-text">
+                                    <h5>Telepon</h5>
+                                    <p>+62 889 7158 9438</p>
+                                </div>
+                            </div>
+                            
+                            <div class="contact-item">
+                                <div class="contact-icon">
+                                    <i class="fas fa-envelope"></i>
+                                </div>
+                                <div class="contact-text">
+                                    <h5>Email</h5>
+                                    <p>Abhirajagiovannicompany@gmail.com</p>
+                                </div>
+                            </div>
+                            
+                            <div class="contact-item">
+                                <div class="contact-icon">
+                                    <i class="fas fa-clock"></i>
+                                </div>
+                                <div class="contact-text">
+                                    <h5>Jam Kerja</h5>
+                                    <p>Senin - Jumat: 08.00 - 17.00</p>
+                                    <p>Sabtu: 09.00 - 14.00</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-7" data-aos="fade-left">
+                        <form class="contact-form">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" placeholder="Nama Lengkap">
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="email" class="form-control" placeholder="Email">
+                                </div>
+                            </div>
+                            <input type="text" class="form-control" placeholder="Subjek">
+                            <textarea class="form-control" placeholder="Pesan"></textarea>
+                            <button type="submit" class="btn-contact">Kirim Pesan</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+@endsection
